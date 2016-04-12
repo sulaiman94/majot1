47acf81e3270ba4cafa33b8a53d9e9a058e08fe5
Sulaiman Aloraini <sulaimanoraini@yahoo.com>
2016-04-12 10:07:18 +0300

    Added files via upload

---
+++ part05/contact.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<?php 
+
+
+print ("Thank you ".$_POST["name"].", your message was send to us and will respond you on ".$_POST["email"].". Here is the content of your message:
+".$_POST["message"]." The Dronemap Team ") ;
+?>
+		
+		
+
+
+</body>
+</html>
